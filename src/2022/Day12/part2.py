28e3157415e02179aa485c43f07b36d3c1747059
Nripesh Manandhar <nripeshmanandhar@gmail.com>
2022-12-12 22:05:00 -0500

    Complete 2022 Day 12 Part 1 and 2

---
+++ src/2022/Day12/part2.py
@@ -0,0 +1,82 @@
+from ...common.input_reader import read_input
+import heapq
+
+
+input_lines = read_input("src/2022/Day12/test.txt")
+
+def char_to_int(lchar):
+    return ord(lchar) - ord('a')
+
+the_map = []
+
+num_rows = 0
+start_index = (-1, -1)
+
+for input_line in input_lines:
+    input_line = input_line.strip()
+
+    the_map.append(list(input_line))
+    num_rows += 1
+
+    for i, height in enumerate(the_map[-1]):
+        if height == 'S':
+            the_map[-1][i] = 'a'
+        if height == 'E':
+            the_map[-1][i] = 'z'
+            start_index = (num_rows - 1, i)
+
+the_map_num = []
+
+for j, row in enumerate(the_map):
+    the_map_num.append([])
+    for i, height in enumerate(row):
+        height_num = char_to_int(height)
+        the_map_num[-1].append(height_num)
+
+next_squares_list = []
+
+for j, row in enumerate(the_map_num):
+    next_squares_list.append([])
+    for i, height in enumerate(row):
+        next_squares_list[-1].append([])
+        
+        if i > 0 and row[i-1] >= height - 1:
+            next_squares_list[-1][-1].append((j, i-1))
+        if i < len(row)-1 and row[i+1] >= height - 1:
+            next_squares_list[-1][-1].append((j, i+1))
+        if j > 0 and the_map_num[j-1][i] >= height - 1:
+            next_squares_list[-1][-1].append((j-1, i))
+        if j < num_rows-1 and the_map_num[j+1][i] >= height - 1:
+            next_squares_list[-1][-1].append((j+1, i))
+
+final_distances = dict()
+
+
+sptSet = [[False] * len(the_map_num[0]) for _ in the_map_num]
+
+squares_heap = []
+
+for j, row in enumerate(the_map):
+    for i, height in enumerate(row):
+        if (j, i) == start_index:
+            heapq.heappush(squares_heap, (0, j, i))
+        else:
+            heapq.heappush(squares_heap, (999999, j, i))
+
+while len(squares_heap) != 0:
+    distance, j, i = heapq.heappop(squares_heap)
+
+    for next_j, next_i in next_squares_list[j][i]:
+        # check if next is in squares_heap
+        for heap_index, (heap_d, heap_j, heap_i) in enumerate(squares_heap):
+            if (heap_j, heap_i) == (next_j, next_i):
+                # update distance
+                if distance + 1 < heap_d:
+                    squares_heap.pop(heap_index)
+                    heapq.heapify(squares_heap)
+                    heapq.heappush(squares_heap, (distance + 1, heap_j, heap_i))
+                    if the_map_num[next_j][next_i] == 0:
+                        final_distances[(next_j, next_i)] = distance + 1
+                break
+
+print(min(final_distances.values()))
